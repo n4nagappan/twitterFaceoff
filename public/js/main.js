8523ed6482ca4f1a7d4af966d56acd1cc137e3f5
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-07-23 21:01:03 +0800

    Fetch twitter handle data using Jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
-function TwitController($scope) { 
-        $scope.twitData = [
-      {handle:'handle1', followers:10, tweets: 99},
-      {handle:'handle2', followers:30, tweets: 88}];       
+//function TwitController($scope) { 
+//        $scope.twitData = [
+//      {handle:'handle1', followers:10, tweets: 99},
+//      {handle:'handle2', followers:30, tweets: 88}];       
+//}
+
+function loadChart(handleData1, handleData2)
+{
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ]);
+
+    var options = {
+      title: 'Company Performance'
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart'));
+    chart.draw(data, options);
+    }
 }
+
+handle = "imraina";
+$.getJSON("http://livecat.cloudapp.net/timeline?handle="+handle,function(){
+    console.log(arguments);
+});
